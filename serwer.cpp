4f6bc9aaeb1a6c2161201698fc88e169ec224188
Witold Andraszyk <wandraszyk73@gmail.com>
2022-01-07 17:41:33 +0100

    Prawie poprawny serwer

--- serwer.cpp
+++ serwer.cpp
@@ -13,14 +13,145 @@
 
 #define MAX_CLIENTS 100
 #define BUFFER_SZ 2048
+#define NAME_LEN 32
 
 using namespace std;
 
 static atomic_uint cli_count(0);
 static int uid = 10;
 
+// Struktura klienta
+typedef struct{
+    struct sockaddr_in address;
+    int sockfd;
+    int uid;
+    char name[NAME_LEN];
+} client_t;
+
+client_t* clients[MAX_CLIENTS];
+
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void str_overwrite_stdout(){
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+void str_trim_lf(char* arr, int length){
+    for(int i=0; i<length; i++){
+        if(arr[i] == '\n'){
+            arr[i] = '\0';
+            break;
+        }
+    }
+}
+
+void queue_add(client_t* cl){
+    pthread_mutex_lock(&clients_mutex);
+
+    for(int i=0; i<MAX_CLIENTS; i++){
+        if(!clients[i]){
+            clients[i] = cl;
+            break;
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+void queue_remove(int uid){
+    pthread_mutex_lock(&clients_mutex);
+
+    for(int i=0; i<MAX_CLIENTS; i++){
+        if(clients[i]){
+            if(clients[i]->uid == uid){
+                clients[i] = NULL;
+                break;
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
 void print_ip_addr(struct sockaddr_in addr){
     printf("%d.%d.%d.%d", addr.sin_addr.s_addr & 0xff, (addr.sin_addr.s_addr & 0xff00) >> 8, (addr.sin_addr.s_addr & 0xff0000) >> 16, (addr.sin_addr.s_addr & 0xff000000) >> 24);
+}
+
+void send_message(char* s, int uid){
+    pthread_mutex_lock(&clients_mutex);
+
+    for(int i=0; i<MAX_CLIENTS; i++){
+        if(clients[i]){
+            if(clients[i]->uid != uid){
+                if(write(clients[i]->sockfd, s, strlen(s) < 0)){
+                    printf("ERROR: blad wysylania wiadomosci\n");
+                    break;
+                }
+            }
+        }
+    }
+
+    pthread_mutex_unlock(&clients_mutex);
+}
+
+void* handle_client(void* arg){
+    char buffer[BUFFER_SZ];
+    char name[NAME_LEN];
+    int leave_flag = 0;
+    cli_count++;
+
+    client_t* cli = (client_t*)arg;
+
+    //Nazwa klienta
+    if(recv(cli->sockfd, name, NAME_LEN, 0) <= 0 || strlen(name) < 2 || strlen(name) >= NAME_LEN - 1){
+        printf("Wprowadz poprawna nazwe\n");
+        leave_flag = 1;
+    }
+    else{
+        strcpy(cli->name, name);
+        sprintf(buffer, "%s dolaczyl do serwera\n", cli->name);
+        printf("%s", buffer);
+        send_message(buffer, cli->uid);
+    }
+
+    bzero(buffer, BUFFER_SZ);
+
+    while(1){
+        if(leave_flag){
+            break;
+        }
+
+        int receive = recv(cli->sockfd, buffer, BUFFER_SZ, 0);
+
+        if(receive > 0){
+            if(strlen(buffer) > 0){
+                send_message(buffer, cli->uid);
+                str_trim_lf(buffer, strlen(buffer));
+                printf("%s -> %s", buffer, cli->name);
+            }
+        }
+        else if(receive = 0 || strcmp(buffer, "exit") == 0){
+            sprintf(buffer, "%s has left\n", cli->name);
+            printf("%s", buffer);
+            send_message(buffer, cli->uid);
+            leave_flag = 1;
+        }
+        else{
+            printf("ERROR: -1\n");
+            leave_flag = 1;
+        }
+
+        bzero(buffer, BUFFER_SZ);
+    }
+
+    close(cli->sockfd);
+    queue_remove(cli->uid);
+    free(cli);
+    cli_count--;
+    pthread_detach(pthread_self());
+
+    return NULL;
 }
 
 int main(int argc, char **argv){
@@ -71,6 +202,7 @@ int main(int argc, char **argv){
         socklen_t clilen = sizeof(cli_addr);
         connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
 
+        //Sprawdzenie limitu klientów
         if((cli_count + 1) == MAX_CLIENTS){
             printf("Osiągnięto limit. Połączenie odrzucone.\n");
             print_ip_addr(cli_addr);
@@ -78,11 +210,19 @@ int main(int argc, char **argv){
             continue;
         }
 
+        //Ustawienia klienta
+        client_t* cli = (client_t*)malloc(sizeof(client_t));
+        cli->address = cli_addr;
+        cli->sockfd = connfd;
+        cli->uid = uid++;
+
+        //Dodawanie klienta
+        queue_add(cli);
+        pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+        //
+        sleep(1);
     }
-
-
-
-
 
     return EXIT_SUCCESS;
 }
